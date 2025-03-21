cf111ea0a04c6c535df5786024cedf4e37d3d19c
Ammar-Sharif <ammarsharif377@gmail.com>
2025-03-21 16:07:31 +0500

    firebase with auth and mongo setup

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from './schemas/user.schema';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async findByEmail(email: string): Promise<User | null> {
-    return this.userModel.findOne({ email }).lean().exec();
+  async findByUid(_id: string): Promise<User | null> {
+    return this.userModel.findOne({ _id }).exec();
   }
-  // We use the exec() method to execute the query and return a promise. because the findOne() method returns a Query object, not a promise. we use it with other methods also like find(), findById(), etc.
-  // The lean() method is used to convert the mongoose document into a plain JavaScript object. This method is used to improve the performance of the query. It is used when you don't want to modify the document before sending it to the client. 
 
-  async create(user: Partial<User>): Promise<User> {
-    const newUser = new this.userModel(user);
-    return newUser.save();
+  async createUser(userData: {
+    _id: string;
+    email: string | undefined;
+    name: string | undefined;
+    photoURL: string;
+  }) {
+    return this.userModel.create(userData);
+  }
+
+  async updateUser(_id: string, updateData: Partial<User>) {
+    return this.userModel.findOneAndUpdate({ _id }, updateData, { new: true });
   }
 }
