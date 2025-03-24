b24905ba5decff106328065477bec0b43f540892
Ammar-Sharif <ammarsharif377@gmail.com>
2025-03-24 12:21:08 +0500

    Added Docker and GitHub Actions

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -10,7 +10,7 @@ export class FirebaseService {
     this.firebaseApp = admin.initializeApp({
       credential: admin.credential.cert({
         projectId: this.configService.get<string>('FIREBASE_PROJECT_ID'),
-        clientEmail: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+        clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
         privateKey: this.configService
           .get<string>('FIREBASE_PRIVATE_KEY')
           ?.replace(/\\n/g, '\n'),
